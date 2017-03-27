fc565483577a1e4d9615027659b493af3f69aa29
Laura Gerhardt <laura.gerhardt@gsa.gov>
2017-03-27 17:44:03 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const Trello = require("node-trello");
+require('dotenv').config();
+const env = process.env;
+
+const Moment = require('moment');
+const MomentRange = require('moment-range');
+
+const moment = MomentRange.extendMoment(Moment);
+
+const trello = new Trello(env.TRELLO_API_KEY, env.TRELLO_API_TOK);
+
+
+const boardID = env.BOARD_ID;
+
+const startDate = moment('2017-03-20')
+, endDate   = moment('2017-03-24')
+, weekRange = moment().range(startDate, endDate);
+
+console.log("Issue Tracker Weekly Update:");
+
+// URL arguments are passed in as an object.
+trello.get(`/1/boards/${boardID}/cards`, { actions: ['createCard', 'updateCard'] }, function(err, data) {
+  if (err) throw err;
+  // console.log(data);
+
+  const newlyCreated = [];
+  const movedtoDone = [];
+  data.forEach(card =>{
+    //Card creation
+    const createdDate = moment(card.actions[card.actions.length-1].date);
+    if(weekRange.contains(createdDate)){
+      newlyCreated.push(card);
+    };
+
+    //Cards resolved
+    if(card.actions.length > 0){
+      card.actions.forEach(action =>{
+        if(action.data.listAfter && action.data.listAfter.name == 'Resolved'  && action.type == 'updateCard'){
+          const resolvedDate = moment(action.date);
+          if(weekRange.contains(resolvedDate)){
+            movedtoDone.push(card);
+          };
+        }
+      });
+    }
+
+  });
+  console.log(newlyCreated.length+" Issues created this week");
+  console.log(movedtoDone.length+" Issues resolved this week");
+
+});
+
+trello.get(`/1/boards/${boardID}/lists?cards=open&card_fields=name`, function(err, data) {
+  if (err) throw err;
+  data.forEach(list=>{
+    if(list.name == 'Waiting' || list.name == 'Blocked'){
+      console.log(list.cards.length + " Issues " + list.name);
+    }
+  })
+
+});
